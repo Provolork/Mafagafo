77f02cd699e50e72ea9fce0af3217d861c45e582
Provolork <provolork@gmail.com>
2021-03-02 15:56:49 -0300

    Initial commit

---
+++ Assets/Monster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+public int Vida = 10;
+public int Vel = 1;
+    public List<GameObject> pointlist = new List<GameObject>() { };
+    public int Cont = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextPoint();
+       Debug.Log("Monstro,Vida: " + Vida + "e Vel: " + Vel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(pointlist[Cont]);  
+    }
+
+    GameObject NextPoint()
+    {
+        Cont++;
+        GameObject nextpoint = pointlist[Cont];
+        return nextpoint;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ponto")
+        {
+            NextPoint();
+        }
+    }
+
+    void Movement(GameObject _nextpoint)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, _nextpoint.transform.position, Vel * Time.deltaTime);
+    }
+
+}
---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject Monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        Instantiate(Monster, transform.position, transform.rotation);
+    }
+}
