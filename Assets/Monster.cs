8f4e96913768a65e3df5b39d94767c43768a7617
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2021-04-02 14:37:04 -0300

    Bug Fixes
    - Consertei o monster
    - Consertei o spawnPoint

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-public int Vida = 10;
-public int Vel = 1;
-    public List<GameObject> pointlist = new List<GameObject>() { };
-    public int Cont = 0;
+    public int Vida = 10;
+    public int Vel = 1;
+    public List<Transform> pointlist = new List<Transform>() { };
+    public GameObject pointHolder;
+    public int Cont = -1;
+
     // Start is called before the first frame update
     void Start()
     {
+        pointHolder = GameObject.FindGameObjectWithTag("PointHolder");
+        FindPoints();
         NextPoint();
        Debug.Log("Monstro,Vida: " + Vida + "e Vel: " + Vel);
     }
@@ -21,24 +25,46 @@ public int Vel = 1;
         Movement(pointlist[Cont]);  
     }
 
-    GameObject NextPoint()
+    
+    //Quando colidir com um trigger...
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Cont++;
-        GameObject nextpoint = pointlist[Cont];
-        return nextpoint;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+        //Se a tag for ponto...
         if (collision.gameObject.tag == "Ponto")
         {
+            //Procura o próximo e vai naquela direção
             NextPoint();
         }
     }
-
-    void Movement(GameObject _nextpoint)
+    //Procura o próximo ponto
+    Transform NextPoint()
+    {
+        Cont++;
+        Transform nextpoint = pointlist[Cont];
+        return nextpoint;
+    }
+    //Vai em direção ao ponto atual
+    void Movement(Transform _nextpoint)
     {
         transform.position = Vector2.MoveTowards(transform.position, _nextpoint.transform.position, Vel * Time.deltaTime);
+    }
+
+    public void FindPoints()
+    {
+        //Limpa a lista
+        pointlist.Clear();
+
+        //Encontra os pontos
+        foreach (Transform child in pointHolder.transform)
+        {
+            pointlist.Add(child);
+        }
+
+        //Coloca os pontos em ordem
+        pointlist.Sort(delegate (Transform go1, Transform go2)
+        {
+            return go1.transform.GetSiblingIndex().CompareTo(go2.transform.GetSiblingIndex());
+        });
     }
 
 }
